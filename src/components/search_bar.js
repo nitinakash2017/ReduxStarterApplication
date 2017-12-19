19fa531a406d274f6c786d84e135b26b62056030
Nitin Akash <nitinakash@Nitins-MacBook-Air.local>
2017-12-19 15:31:39 -0400

    Added on input change event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 class SearchBar extends React.Component{
   render(){
-    return <input />;
+    return <input onChange={this.onInputChange} />;
+  }
+
+  onInputChange(event){
+    console.log(event.target.value)
   }
 }
 
