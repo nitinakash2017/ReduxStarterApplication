d5f1f35646f25cd4606190371ee0af32f23ea306
Nitin Akash <nitinakash@Nitins-MacBook-Air.local>
2017-12-19 15:46:15 -0400

    Changed Handler to inline Handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 class SearchBar extends React.Component{
   render(){
-    return <input onChange={this.onInputChange} />;
+    return <input onChange={(event) => console.log(event.target.value)} /*{this.onInputChange}*/ />;
   }
 
-  onInputChange(event){
-    console.log(event.target.value)
-  }
+  //onInputChange(event){
+  //  console.log(event.target.value)
+  //}
 }
 
 export default SearchBar;
